31060e9b1ace2e858b97c384229a19448d477e0c
302A-08\cre <a>
2024-07-09 16:09:26 +0900

    7月９日

--- works/work2/q5.php
+++ works/work2/q5.php
@@ -1,3 +1,8 @@
 <?php
 // 配列の全ての要素を2倍にして表示するコードを記述してください
 $numbers = array(1, 2, 3, 4, 5);
+echo $numbers[0] * 2;
+echo $numbers[1] * 2;
+echo $numbers[2] * 2;
+echo $numbers[3] * 2;
+echo $numbers[4] * 2;
--- works/work2/q6.php
+++ works/work2/q6.php
@@ -1,4 +1,9 @@
 <?php
 
-// 以下の配列をの要素のうち文字数が偶数のもののみ表示するループを書いてください
+// 以下の配列を要素のうち文字数が偶数のもののみ表示するループを書いてください
 $fruits = array("Apple", "Banana", "Cherry");
+for ($i = 0; $i < count($fruits); $i++) {
+    if (mb_strlen($fruits[$i]) % 2 == 0) {
+        echo $fruits[$i];
+    }
+}
